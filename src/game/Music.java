4c28cde16f7e441f760a916a4239802449c49d2f
unknown <danieltsema@gmail.com>
2018-10-29 19:36:37 -0400

    Changed music file access

--- src/game/Music.java
+++ src/game/Music.java
@@ -1,11 +1,21 @@
 package game;
 
+import java.io.IOException;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class Music extends Thread { 
 	runMusic run;
   public static boolean done = false;
 
   public void run() {
 	  run = new runMusic();
-	  run.play();
+	  try {
+		run.play();
+	} catch (UnsupportedAudioFileException e) {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
    }
 }
--- src/game/runMusic.java
+++ src/game/runMusic.java
@@ -1,8 +1,7 @@
 package game;
 
-import java.io.File;
 import java.io.IOException;
-
+import java.net.URL;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -14,8 +13,6 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class runMusic { 
 
-    private String filename;
-
     private Position curPosition;
 
     private final int EXTERNAL_BUFFER_SIZE = 524288; // 128Kb 
@@ -25,76 +22,60 @@ public class runMusic {
     };
 
     public runMusic() { 
-    	filename = "music.wav";
         curPosition = Position.NORMAL;
     } 
 
     public runMusic(Position p) { 
-        filename = "music.wav";
         curPosition = p;
     } 
     
-    public void play(){
-    File soundFile = new File(filename);
-    if (!soundFile.exists()) { 
-        System.err.println("Wave file not found: " + filename);
-        return;
-    } 
-
-    AudioInputStream audioInputStream = null;
-    try { 
-        audioInputStream = AudioSystem.getAudioInputStream(soundFile);
-    } catch (UnsupportedAudioFileException e1) { 
-        e1.printStackTrace();
-        return;
-    } catch (IOException e1) { 
-        e1.printStackTrace();
-        return;
-    } 
-
-    AudioFormat format = audioInputStream.getFormat();
-    SourceDataLine auline = null;
-    DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-
-    try { 
-        auline = (SourceDataLine) AudioSystem.getLine(info);
-        auline.open(format);
-    } catch (LineUnavailableException e) { 
-        e.printStackTrace();
-        return;
-    } catch (Exception e) { 
-        e.printStackTrace();
-        return;
-    } 
-
-    if (auline.isControlSupported(FloatControl.Type.PAN)) { 
-        FloatControl pan = (FloatControl) auline
-                .getControl(FloatControl.Type.PAN);
-        if (curPosition == Position.RIGHT) 
-            pan.setValue(1.0f);
-        else if (curPosition == Position.LEFT) 
-            pan.setValue(-1.0f);
-    } 
-
-    auline.start();
-    int nBytesRead = 0;
-    byte[] abData = new byte[EXTERNAL_BUFFER_SIZE];
-
-    try { 
-        while (nBytesRead != -1) { 
-            nBytesRead = audioInputStream.read(abData, 0, abData.length);
-            if (nBytesRead >= 0) 
-                auline.write(abData, 0, nBytesRead);
-            
-        } 
-    } catch (IOException e) { 
-        e.printStackTrace();
-        return;
-    } finally { 
-    	Music.done=true;
-        auline.drain();
-        auline.close();
-    	} 
-
-    } 
+    public void play() throws UnsupportedAudioFileException, IOException{
+    	URL url = Game.class.getResource("/music/music.wav");
+    	AudioInputStream ais = AudioSystem.getAudioInputStream(url);
+    	
+	    AudioFormat format = ais.getFormat();
+	    SourceDataLine auline = null;
+	    DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+	
+	    try { 
+	        auline = (SourceDataLine) AudioSystem.getLine(info);
+	        auline.open(format);
+	    } catch (LineUnavailableException e) { 
+	        e.printStackTrace();
+	        return;
+	    } catch (Exception e) { 
+	        e.printStackTrace();
+	        return;
+	    } 
+	
+	    if (auline.isControlSupported(FloatControl.Type.PAN)) { 
+	        FloatControl pan = (FloatControl) auline
+	                .getControl(FloatControl.Type.PAN);
+	        if (curPosition == Position.RIGHT) 
+	            pan.setValue(1.0f);
+	        else if (curPosition == Position.LEFT) 
+	            pan.setValue(-1.0f);
+	    } 
+	
+	    auline.start();
+	    int nBytesRead = 0;
+	    byte[] abData = new byte[EXTERNAL_BUFFER_SIZE];
+	
+	    try { 
+	        while (nBytesRead != -1) { 
+	            nBytesRead = ais.read(abData, 0, abData.length);
+	            if (nBytesRead >= 0) 
+	                auline.write(abData, 0, nBytesRead);
+	            
+	        } 
+	    } catch (IOException e) { 
+	        e.printStackTrace();
+	        return;
+	    } finally { 
+	    	Music.done=true;
+	        auline.drain();
+	        auline.close();
+	    	} 
+	
+	    } 
 }
